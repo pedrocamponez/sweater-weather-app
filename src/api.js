f1b0f686a227f90bc5f2d894f8f09186ee476180
unknown <camponezpedro@gmail.com>
2023-12-11 14:39:33 -0300

    Complete app coded

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '0d991e6f16msh2f8ce9259e785fep12c317jsn8518d6bedd63',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "95bb342e9fe1dc99b3c28e0dd593acd5"
